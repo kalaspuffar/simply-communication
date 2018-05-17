69001e705c5bc78b4e624153df4d7b73ee81e524
Daniel Persson <mailto.woden@gmail.com>
2018-05-17 22:30:39 +0200

    Add travis function to test our site.

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Auto-Deploy Pull Request
+
+# If this isn't a pull request, abort.
+if [ "${TRAVIS_EVENT_TYPE}" != "pull_request" ]; then
+  echo "This only runs on pull_request events. Event was $TRAVIS_EVENT_TYPE."
+  exit
+fi
+
+# If there were build failures, abort
+if [ "${TRAVIS_TEST_RESULT}" = "1" ]; then
+  echo "Deploy aborted, there were build/test failures."
+  exit
+fi
+
+ftp-upload -h ${GOOGIE_HOST} -u ${GOOGIE_PASS} -d public_html -b -v build/es5-bundled/*
