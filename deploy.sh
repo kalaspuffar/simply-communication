b0ceb1e2bb448bc67a727a2de0443f51f521b171
Daniel Persson <mailto.woden@gmail.com>
2018-05-17 23:38:47 +0200

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -e
 
-# Auto-Deploy Pull Request
-
-# If this isn't a pull request, abort.
-if [ "${TRAVIS_EVENT_TYPE}" != "pull_request" ]; then
-  echo "This only runs on pull_request events. Event was $TRAVIS_EVENT_TYPE."
-  exit
-fi
-
 # If there were build failures, abort
 if [ "${TRAVIS_TEST_RESULT}" = "1" ]; then
   echo "Deploy aborted, there were build/test failures."
   exit
 fi
 
-ncftpput -R -v -u $GOOGIEHOST_USER -p $GOOGIEHOST_PASS $GOOGIEHOST_HOST public_html build/es5-bundled
+ncftpput -R -v -u "${GOOGIEHOST_USER}" -p "${GOOGIEHOST_PASS}" $GOOGIEHOST_HOST public_html build/es5-bundled
